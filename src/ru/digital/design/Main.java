8b1070065faeeaafe0e3d46c22ec6f60b3fcafd3
Alexandr Pogodaev <trollegon@narod.ru>
2023-12-11 17:52:41 +0800

    User class added

--- src/ru/digital/design/Main.java
+++ src/ru/digital/design/Main.java
@@ -5,5 +5,10 @@ public class Main {
         for (Columns coulmn : Columns.values()) {
             System.out.println(coulmn + " имеет значение "+ coulmn.getColumnName());
         }
+        User user = new User.UserBulder().id(1).lastName("Вася").age(30).cost(3.5).active(true)
+                .build();
+        System.out.println(user);
+        user = new User.UserBulder().id(2).lastName("Петя").build();
+        System.out.println(user);
     }
 }
