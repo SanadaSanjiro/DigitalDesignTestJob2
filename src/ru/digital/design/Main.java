a4684c78b8e6c9a3db4dcae8670e2748421153d9
Alexandr Pogodaev <trollegon@narod.ru>
2023-12-14 17:03:57 +0800

    User class improved

--- src/ru/digital/design/Main.java
+++ src/ru/digital/design/Main.java
@@ -1,14 +1,23 @@
 package ru.digital.design;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        for (Columns coulmn : Columns.values()) {
-            System.out.println(coulmn + " имеет значение "+ coulmn.getColumnName());
+        for (Column column : Column.values()) {
+            System.out.println(column + " имеет значение "+ column.getColumnName()
+                    + " и класс " + column.getColumnClass());
         }
-        User user = new User.UserBulder().id(1).lastName("Вася").age(30).cost(3.5).active(true)
+        User user = new User.UserBuilder()
+                .addColumn(Column.ID, Long.valueOf(1))
+                //.addColumn(Column.LASTNAME, "Вася")
+                .addColumn(Column.AGE, Long.valueOf(30))
+                //.addColumn(Column.COST, Double.valueOf(2.5))
+                .addColumn(Column.ACTIVE, Boolean.valueOf(true))
                 .build();
-        System.out.println(user);
-        user = new User.UserBulder().id(2).lastName("Петя").build();
-        System.out.println(user);
+        Map<Column, Object> map = user.getMap();
+        for(Column column: Column.values()) {
+            System.out.println(column + ": " + map.get(column));
+        }
     }
 }
--- src/ru/digital/design/User.java
+++ src/ru/digital/design/User.java
@@ -1,70 +1,33 @@
 package ru.digital.design;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class User {
-    private final Long id;
-    private final String lastName;
-    private final Long age;
-    private final Double cost;
-    private final Boolean active;
-
-    public static class UserBulder
+    private final Map<Column, Object> map;
+    public static class UserBuilder
     {
-        private Long id = null;
-        private String lastName = null;
-        private Long age = null;
-        private Double cost = null;
-        private Boolean active = null;
-
-        public UserBulder id(long val)
+        private final Map<Column, Object> temporaryMap = new HashMap<>();
+        public UserBuilder() {
+            for (Column column : Column.values()) {
+                temporaryMap.put(column, null);
+            }
+        }
+        public UserBuilder addColumn(Column column, Object object)
         {
-            this.id = val;
+
+            temporaryMap.put(column, object);
             return this;
         }
-
-        public UserBulder lastName(String val) {
-            this.lastName = val;
-            return this;
-        }
-
-        public UserBulder age(long val) {
-            this.age = val;
-            return this;
-        }
-
-        public UserBulder cost(double val) {
-            this.cost = val;
-            return this;
-        }
-
-        public UserBulder active(boolean val) {
-            this.active = val;
-            return this;
-        }
-
-        public User build() {
-            return new User(this);
-        }
+        public User build() { return new User(this); }
     }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", cost=" + cost +
-                ", active=" + active +
-                '}';
-    }
-
     private User() {
         throw new IllegalArgumentException("Wrong constructor call. Use Builder!");
     }
-    private User(UserBulder builder) {
-        id = builder.id;
-        lastName = builder.lastName;
-        age = builder.age;
-        cost = builder.cost;
-        active = builder.active;
+    private User(UserBuilder builder) {
+        this.map = builder.temporaryMap;
+    }
+    public Map<Column, Object> getMap() {
+        return map;
     }
 }
